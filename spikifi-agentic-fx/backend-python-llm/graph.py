8d9d8fcb93beedd9ade31402b0393b62acc57c86
surusara <jaiswal.surendra@gmail.com>
2025-07-07 00:10:05 +0800

    Update graph.py

--- spikifi-agentic-fx/backend-python-llm/graph.py
+++ spikifi-agentic-fx/backend-python-llm/graph.py
@@ -1,10 +1,19 @@
 from langgraph.graph import StateGraph
+from pydantic import BaseModel
+from typing import Optional, Any
+
 from agents.main_agent import create_main_agent
 from agents.volume_agent import create_volume_agent
 from agents.market_agent import create_market_agent
 
+class MemoryState(BaseModel):
+    input: str
+    memory: Optional[str] = ""
+    output: Optional[Any] = None
+
 def build_graph():
-    builder = StateGraph()
+    builder = StateGraph(MemoryState)
+
     builder.add_node("main", create_main_agent())
     builder.add_node("get_volume_forecast", create_volume_agent())
     builder.add_node("get_market_events", create_market_agent())
